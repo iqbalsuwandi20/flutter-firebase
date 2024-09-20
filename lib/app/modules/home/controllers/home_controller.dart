c4f5df31067178aa02c3c85b9e125f2b7e8c7b43
iqbalsuwandi20 <iqbalsuwandi20@gmail.com>
2024-09-20 14:43:20 +0700

    get all notes

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,3 +1,19 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
 
-class HomeController extends GetxController {}
+class HomeController extends GetxController {
+  FirebaseAuth auth = FirebaseAuth.instance;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+
+  Stream<QuerySnapshot<Map<String, dynamic>>> streamNotes() async* {
+    String uid = auth.currentUser!.uid;
+
+    yield* firestore
+        .collection("users")
+        .doc(uid)
+        .collection("notes")
+        .orderBy("createdAt")
+        .snapshots();
+  }
+}
--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -27,12 +28,61 @@ class HomeView extends GetView<HomeController> {
           ),
         ],
       ),
-      body: const Center(
-        child: Text(
-          'HomeView is working',
-          style: TextStyle(fontSize: 20),
-        ),
-      ),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+          stream: controller.streamNotes(),
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return Center(
+                child: CircularProgressIndicator(
+                  color: Colors.blue[600],
+                ),
+              );
+            }
+
+            if (snapshot.data?.docs.length == null || snapshot.data == null) {
+              return const Center(
+                child: Text(
+                  "Belum ada data",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+              );
+            }
+
+            return ListView.builder(
+              itemCount: snapshot.data!.docs.length,
+              padding: const EdgeInsets.all(5),
+              itemBuilder: (context, index) {
+                var docNote = snapshot.data!.docs[index];
+                Map<String, dynamic> note = docNote.data();
+                return ListTile(
+                  leading: CircleAvatar(
+                    backgroundColor: Colors.blue[600],
+                    child: Text(
+                      "${index + 1}",
+                      style: const TextStyle(color: Colors.white),
+                    ),
+                  ),
+                  title: Text(
+                    "${note["title"]}",
+                    style: TextStyle(
+                      color: Colors.blue[600],
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  subtitle: Text(
+                    "${note["description"]}",
+                    style: TextStyle(color: Colors.blue[600]),
+                  ),
+                  trailing: IconButton(
+                      onPressed: () {},
+                      icon: Icon(
+                        Icons.delete_forever_outlined,
+                        color: Colors.blue[600],
+                      )),
+                );
+              },
+            );
+          }),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           Get.toNamed(Routes.ADD_NOTE);
