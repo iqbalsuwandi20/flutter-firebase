1e647c576f941f97c08c322f5341534a86ace37d
iqbalsuwandi20 <iqbalsuwandi20@gmail.com>
2024-09-23 19:11:09 +0700

    upload image

--- lib/app/modules/profile/controllers/profile_controller.dart
+++ lib/app/modules/profile/controllers/profile_controller.dart
@@ -1,14 +1,18 @@
+import 'dart:io';
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart' as s;
 
 import '../../../routes/app_pages.dart';
 
 class ProfileController extends GetxController {
   FirebaseAuth auth = FirebaseAuth.instance;
   FirebaseFirestore firestore = FirebaseFirestore.instance;
+  s.FirebaseStorage storage = s.FirebaseStorage.instance;
 
   TextEditingController emailC = TextEditingController();
   TextEditingController nameC = TextEditingController();
@@ -54,6 +58,22 @@ class ProfileController extends GetxController {
           "name": nameC.text,
           "phone": phoneC.text,
         });
+
+        if (image != null) {
+          String ext = image!.name.split(".").last;
+          await storage
+              .ref(uid)
+              .child("profile.$ext")
+              .putFile(File(image!.path));
+
+          String profileLink =
+              await storage.ref(uid).child("profile.$ext").getDownloadURL();
+
+          await firestore
+              .collection("users")
+              .doc(uid)
+              .update({"profilePicture": profileLink});
+        }
 
         if (passC.text.isNotEmpty) {
           await auth.currentUser!.updatePassword(passC.text);
